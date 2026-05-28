1bd2d1bdd57c494fe6d71f296c7735e117303090
Dominykas Ulevič <dominikulevic99@gmail.com>
2026-05-28 21:57:15 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+print("Hello World😺")
+print("*"*10)
+print("Welcome")
+# shortcut to access shift+cntr+p
+# In this step I understood how to find a problems in a code (ctr+shift+m)
+print(2+2)
+x = 1
+y = 2
+# In this section I learned how to change formating and set it by default to python 3.14
+print(f"{x} + {y} = {x+y}")
+# Now we are getting shortcut in to run code function which is "ctrl+p"
+# "
+course = "Python Programming"
+print(len(course))
+# This function calculates the length of the "course".
+print(course[0])
+print(course[-1])
+# This function takes letter from the "course".
+print(course[0:6])
+# This function takes the first 6 letters from the "course".
+print(course[7:18])
+# This function takes the letters from the 7th to the 18th of the "course".
+# Back slash "\" is essential for the code to work, it is used to break the line and continue it on the next line."
+course = "Python Programming"
+print(course.upper())
+# This function changes all the letters in the "course" to uppercase.
+print(course.lower())
+# This function changes all the letters in the "course" to lowercase.
+print(course.title())
+# This function changes the first letter of each word in the "course" to uppercase.
+print(10 ^ 2)
+print(10**3)
+print(10 % 3)
+x = 10
+y = 10
+x = x+3
+y += 3
+print(x)
+print(y)
